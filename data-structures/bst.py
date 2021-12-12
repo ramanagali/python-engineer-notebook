57b5ce0daccba977fd61631cf3bcb81c97674cb8
ramanagali <ramana.gali@gmail.com>
2021-12-13 01:50:07 +0800

    updated data structures

--- data-structures/bst.py
+++ data-structures/bst.py
@@ -39,12 +39,106 @@ class BST:
             else:
                 print('Node is not present in tree')
 
+    def preorder(self):
+
+        print(self.key, end=' ')
+
+        if self.lc:
+            self.lc.preorder()
+        if self.rc:
+            self.rc.preorder()
+
+    def inorder(self):
+
+        if self.lc:
+            self.lc.inorder()
+
+        if self.key:
+            print(self.key, end=' ')
+
+        if self.rc:
+            self.rc.inorder()
+
+    def postorder(self):
+        if self.lc:
+            self.lc.postorder()
+
+        if self.rc:
+            self.rc.postorder()
+
+        if self.key:
+            print(self.key, end=' ')
+
+    def delete(self, data):
+        # if tree is not empty
+        if self.key is None:
+            print('Tree is empty')
+            return
+
+        # it no empty search
+        if data < self.key:
+            if self.lc:
+                self.lc = self.lc.delete(data)
+            else:
+                print('Given node is not present in tree')
+        elif data > self.key:
+            if self.rc:
+                self.rc = self.rc.delete(data)
+            else:
+                print('Given node is not present in tree')
+        # delete
+        else:
+            # check left subt
+            if self.lc is None:
+                temp = self.rc
+                self = None
+                return temp
+            if self.rc is None:
+                temp = self.lc
+                self = None
+                return temp
+            # delete if more more than 1 child nodes
+            node = self.rc  # check smallest value at right side
+            while node.lc:
+                node = node.lc
+            self.key = node.key
+            self.rc = self.rc.delete(node.key)
+            return self
+
+    def min_node(self):
+        curr = self
+        while curr.lc:
+            curr = curr.lc
+        print('node with smallest key is :', curr.key)
+
+    def max_node(self):
+        curr = self
+        while curr.rc:
+            curr = curr.rc
+        print('node with largest key is :', curr.key)
+
 
 if __name__ == '__main__':
     root = BST(10)
 
-    list1 = [6, 3, 1, 6, 4, 93, 3, 7]
+    list1 = [6, 3, 1, 6, 4, 93, 3, 7, 83, 77, 24]
     for i in list1:
         root.insert(i)
 
     root.search(4)
+    print('Pre-order')
+    root.preorder()
+    print()
+    print('Inorder')
+    root.inorder()
+    print()
+    print('Post Order')
+    root.postorder()
+    print()
+
+    root.delete(6)
+    print('After Delete-Pre-order')
+    root.preorder()
+
+    root.min_node()
+    root.max_node()
